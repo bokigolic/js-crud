4c866eacc9d8774018d1ad24427249d1ce70b07e
bokigolic <bokigolic32@gmail.com>
2023-01-11 00:03:31 -0500

    add button submit

--- script.js
+++ script.js
@@ -1,1 +1,34 @@
 console.log("Hello Boki")
+
+const form = document.getElementById("form");
+const name = document.getElementById("name");
+const quantity = document.getElementById("quantity");
+
+// functions
+const handleSubmit = (e) => {
+  e.preventDefault();
+  console.log("Submit")
+}
+
+
+//init funcion
+
+const init = () => {
+  console.log("Initializations...");
+
+  // add event listener
+
+  form.addEventListener("submit", handleSubmit)
+
+
+  console.log("Initialized");
+};
+
+
+
+
+
+
+//start script
+
+init();
